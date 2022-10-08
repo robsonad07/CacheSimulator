051e5c1bffdf15e0de10b1ad4e2625211c8c253b
Robson <robsonad07@gmail.com>
2022-10-08 16:43:19 -0300

    subindo projeto

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cache Simulator
+Simulator of Cache Memory and MMU of ARMv7 systems 
---
+++ main.c
@@ -0,0 +1,26 @@
+#include "lib/cache.h"
+#include "input/input.h"
+#include "lib/mmu.h"
+int main (){
+    unsigned int bufferAddressSize = CountLines("files/address.txt");
+    unsigned int bufferAddress[bufferAddressSize];
+    unsigned int realAddress[bufferAddressSize];
+    CacheMemory cache;
+
+    initMMU();
+    InitCache(&cache);
+
+    LoadFile("files/address.txt",bufferAddress);
+
+    for (int i = 0; i < bufferAddressSize; ++i) {
+        realAddress[i] = handlerMMU(bufferAddress[i]);
+    }
+
+    for (int i = 0; i < bufferAddressSize; ++i) {
+        AddressHandler(&cache,realAddress[i]);
+    }
+
+  PrintCacheLines(&cache);
+
+	return 0;
+}
